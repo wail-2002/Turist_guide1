c2f1816219d79322f61cfeb917fdc57bd9f5de71
wail-2002 <wail-2002@hotmail.com>
2026-08-27 13:34:41 +0200

    Initial commit

---
+++ src/main/java/com/example/turist_guide1/TuristGuide1Application.java
@@ -0,0 +1,13 @@
+package com.example.turist_guide1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuristGuide1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TuristGuide1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/turist_guide1/TuristGuide1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.turist_guide1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TuristGuide1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
